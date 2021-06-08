e49acdf07f9e581cca7675730888633fa9b6733f
harrisonwheeler <wheelerharrison93@gmail.com>
2021-06-08 16:54:14 -0600

    added google font, small styling changes

--- src/components/SportsComponent.vue
+++ src/components/SportsComponent.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="sportsComponent card">
+  <div class="sportsComponent card border border-white">
     <div v-if="!newsProp.urlToImage">
+      <!-- TODO need to find a better solution to this -->
       <h6>No Image</h6>
     </div>
     <div v-else>
-      <img class="card-img" :src="newsProp.urlToImage" alt="">
-      <div class="card-img-overlay img-overlay-hover">
-        <h6 class="text-light display-none">
-          {{ newsProp.title }}
-        </h6>
+      <img class="card-img box-shadow" :src="newsProp.urlToImage" alt="">
+      <div class="card-img-overlay img-overlay-hover d-flex align-items-end text-left text-light pb-1">
+        <div class="row">
+          <div class="cursor opacity border rounded height p-1">
+            {{ newsProp.title }}
+          </div>
+        </div>
+        <!-- TODO lets add the title at the bottom of the card, and have it transition up to reveal the test of the info -->
       </div>
     </div>
   </div>
@@ -34,10 +38,25 @@ export default {
 <style lang="scss" scoped>
 .img-overlay-hover:hover > .display-none{
   display: block;
+}
+
+.box-shadow{
+  box-shadow: 10px 10px 8px 10px #000000;
+}
+
+.opacity{
   background-color: rgba(#000, 0.4);
+}
+
+.height{
+  height: 100%;
 }
 
 .display-none{
   display: none;
 }
+
+.cursor{
+  cursor: pointer
+  }
 </style>
